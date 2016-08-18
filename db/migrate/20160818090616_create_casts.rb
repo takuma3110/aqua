c6a3a3d72734c012f8cb998527a15acd9bf14548
Takuma Saito <nitol5524@gmail.com>
2016-08-18 18:16:15 +0900

    create cast table

--- db/migrate/20160818090616_create_casts.rb
+++ db/migrate/20160818090616_create_casts.rb
@@ -1,7 +1,7 @@
 class CreateCasts < ActiveRecord::Migration
   def change
     create_table :casts do |t|
-
+      t.string :name
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818075724) do
+ActiveRecord::Schema.define(version: 20160818090616) do
+
+  create_table "casts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
